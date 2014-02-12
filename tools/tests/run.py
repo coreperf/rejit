92e86a425bf999e4903ff5228705524b101429ff
Alexandre Rames <alexandre.rames@coreperf.com>
2014-02-12 18:22:29 +0000

    Clean the testing script.
    Cleaning includes:
    - Make it easier to add new options to test.
    - Display commands executed so tests can be manually replayed.

--- tools/tests/run.py
+++ tools/tests/run.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 # Copyright (C) 2013 Alexandre Rames <alexandre@coreperf.com>
+
 # rejit is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
@@ -17,8 +18,9 @@
 import sys
 import os
 from os.path import join, dirname
-import subprocess
 import argparse
+import itertools
+import subprocess
 
 # Import rejit utils.
 dir_tests = dirname(os.path.realpath(__file__))
@@ -28,95 +30,161 @@ while 'SConstruct' not in os.listdir(dir_rejit):
 sys.path.insert(0, join(dir_rejit, 'tools'))
 import utils
 
-parser = argparse.ArgumentParser(description='Run rejit tests.',
+
+
+script_description = \
+'''
+Run rejit tests.
+The tests are run for a all possible combinations of options that are required
+to be tested.
+'''
+
+
+
+def optionify(name):
+  return '--' + name
+
+
+
+# The options that can be tested are abstracted to provide an easy way to add
+# new ones.
+# Build options are options passed to scons, with a syntax like `scons opt=val`
+# Run (short for runtime) options are options passed to the test program and
+# control runtime flags in rejit.
+# See the definition of `test_options` below.
+
+# 'all' is a special value for the options. If specified, all other values of
+# the option are tested.
+class TestOption(object):
+  def __init__(self, option_type, name, help,
+               val_test_choices, val_test_default = None):
+    self.name = name
+    self.option_type = option_type
+    self.help = help
+    self.val_test_choices = val_test_choices
+    if val_test_default is not None:
+      self.val_test_default = val_test_default
+    else:
+      self.val_test_default = val_test_choices[0]
+
+  type_build = 'build'
+  type_run = 'run'
+
+  def is_build_option(self):
+    return self.option_type == TestOption.type_build
+  def is_run_option(self):
+    return self.option_type == TestOption.type_run
+
+class BuildOption(TestOption):
+  option_type = TestOption.type_build
+  def __init__(self, name, help,
+               val_test_choices, val_test_default = None):
+    super(BuildOption, self).__init__(BuildOption.option_type, name, help, val_test_choices, val_test_default)
+  def args_list(self, to_test):
+    res = []
+    if to_test == 'all':
+      for val in self.val_test_choices:
+        if val != 'all':
+          res.append(self.name + '=' + val)
+    else:
+      res.append(to_test)
+    return res
+
+class RunOption(TestOption):
+  option_type = TestOption.type_run
+  def __init__(self, name, help,
+               val_test_choices, val_test_default = None):
+    super(RunOption, self).__init__(RunOption.option_type, name, help, val_test_choices, val_test_default)
+  def args_list(self, to_test):
+    res = []
+    if to_test == 'all':
+      for val in self.val_test_choices:
+        if val != 'all':
+          res.append(optionify(self.name) + '=' + val)
+    else:
+      res.append(to_test)
+    return res
+
+
+
+test_options = [
+  BuildOption('mode', 'Test with the specified build modes.',
+              val_test_choices=['all'] + utils.build_options_modes),
+  BuildOption('simd', 'Test with the specified SIMD configurations.',
+              val_test_choices=['all', 'on', 'off']),
+  RunOption('use_fast_forward', 'Test with the specified configurations for fast-forwarding.',
+            val_test_choices=['all', '1', '0']),
+  RunOption('use_ff_reduce', 'Test with the specified configurations for common substrings extraction.',
+            val_test_choices=['all', '1', '0'])
+]
+
+test_build_options = [opt for opt in test_options if opt.is_build_option()]
+test_run_options = [opt for opt in test_options if opt.is_run_option()]
+
+
+
+def ParserAddTestOptionsArguments(parser, test_opts):
+  for opt in test_opts:
+    parser.add_argument(optionify(opt.name),
+                        choices=opt.val_test_choices,
+                        default=opt.val_test_default,
+                        help=opt.help,
+                        action='store')
+
+
+parser = argparse.ArgumentParser(description=script_description,
                                  formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument('-j', '--jobs', type=int, action='store',
                     default=1,
                     help='Number of jobs to run simultaneously for the *build* commands')
-parser.add_argument('--mode', choices=utils.build_options_modes + ['both'], action='store',
-                    default='both',
-                    help='Test with the specified build modes.')
-parser.add_argument('--simd', choices=['on', 'off', 'both'], action='store',
-                    default='both',
-                    help='Test SIMD with the specified configurations.')
-parser.add_argument('--use_fast_forward', action='store',
-                    choices=['on', 'off', 'both'], default='both',
-                    help='Test with the fast-forward mechanisms for the specified configurations.')
-parser.add_argument('--use_ff_reduce', action='store',
-                    choices=['on', 'off', 'both'], default='both',
-                    help='Test with common substring extraction mechanisms for fast-forward elements.')
+ParserAddTestOptionsArguments(parser, test_options)
 args = parser.parse_args()
 
 
-dir_tests = dirname(os.path.realpath(__file__))
-dir_rejit = dir_tests
-while 'SConstruct' not in os.listdir(dir_rejit):
-  dir_rejit = os.path.realpath(join(dir_rejit, '..'))
-sys.path.insert(0, join(dir_rejit, 'tools'))
-import utils
+# List build and run options applicable for this test run.
+test_build_args_list = map(lambda opt : opt.args_list(args.__dict__[opt.name]), test_build_options)
+test_run_args_list = map(lambda opt : opt.args_list(args.__dict__[opt.name]), test_run_options)
+# List all combinations of build and run options that will be tested.
+test_build_args_combinations = list(itertools.product(*test_build_args_list))
+test_run_args_combinations = list(itertools.product(*test_run_args_list))
 
 
-if args.mode == 'both':
-  build_modes = utils.build_options_modes
-else:
-  build_modes = [args.mode]
 
-if args.simd == 'both':
-  simd_modes = ['on', 'off']
-else:
-  simd_modes = [args.simd]
+options_format=''
+for opt in test_options:
+  options_format += optionify(opt.name) + '={:<' + str(max(map(len, opt.val_test_choices))) + '} '
 
-if args.use_fast_forward == 'both':
-  use_fast_forward_modes = ['on', 'off']
-else:
-  use_fast_forward_modes = [args.use_fast_forward]
 
-if args.use_ff_reduce == 'both':
-  use_ff_reduce_modes = ['on', 'off']
-else:
-  use_ff_reduce_modes = [args.use_ff_reduce]
 
-# Build and run tests in all modes.
-# The automated tests test both with SIMD enabled and disabled for maximum
-# coverage.
-testing_str = "Testing ("
-testing_str += "mode={:<" + str(max(map(len, build_modes))) + "}, "
-testing_str += "simd={:<" + str(max(map(len, simd_modes)) )+ "}, "
-testing_str += "use_fast_forward={:<" + str(max(map(len, use_fast_forward_modes))) + "}, "
-testing_str += "use_ff_reduce={:<" + str(max(map(len, use_ff_reduce_modes))) + "}"
-testing_str += ")...\t"
-for mode in build_modes:
-  for simd_enabled in simd_modes:
-    for ff_enabled in use_fast_forward_modes:
-      for ff_reduce in use_ff_reduce_modes:
-        print testing_str.format(mode, simd_enabled, ff_enabled, ff_reduce),
-        sys.stdout.flush()  # Flush early to tell the user something is running.
-        scons_command = ["scons", "-C", dir_rejit, 'test-rejit', '-j', str(args.jobs),
-            "benchtest=on", "modifiable_flags=on", "mode=%s" % mode, "simd=%s" % simd_enabled]
-        pscons = subprocess.Popen(scons_command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-        scons_ret = pscons.wait()
-        if scons_ret != 0:
-          print 'FAILED'
-          print 'Command:'
-          print ' '.join(scons_command)
-          print 'Output:'
-          scons_output = pscons.communicate()[0]
-          print scons_output
-        else:
-          if ff_enabled == 'on':
-            use_fast_forward = 1
-          else:
-            use_fast_forward = 0
-          ptest = subprocess.Popen([join(utils.dir_build_latest, 'test-rejit'), '--use_fast_forward=%d' % use_fast_forward], stdout=subprocess.PIPE)
-          test_output = ''
-          test_ret = ptest.poll()
-          while test_ret is None:
-            test_output += ptest.communicate()[0]
-            test_ret = ptest.poll()
-          if test_ret != 0:
-            print 'FAILED'
-            print 'Output:'
-            print test_output
-          else:
-            if test_output:
-              print test_output,
+for build_args in test_build_args_combinations:
+  # Build the test executable for the current build options.
+  scons_command = ["scons", "-C", dir_rejit, 'test-rejit',
+                   '-j', str(args.jobs), "benchtest=on", "modifiable_flags=on"]
+  scons_command += build_args
+  print ' '.join(scons_command)
+  p_scons = subprocess.Popen(scons_command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+  scons_ret = p_scons.wait()
+  if scons_ret != 0:
+    print 'Command:'
+    print ' '.join(scons_command)
+    print 'Output:'
+    scons_output = p_scons.communicate()[0]
+    print scons_output
+    sys.exit(scons_ret)
+
+  for run_args in test_run_args_combinations:
+    test_command = [join(utils.dir_build_latest, 'test-rejit')]
+    test_command += run_args
+    print ' '.join(test_command),
+    p_test = subprocess.Popen(test_command, stdout=subprocess.PIPE)
+    test_output = ''
+    test_ret = p_test.poll()
+    while test_ret is None:
+      test_output += p_test.communicate()[0]
+      test_ret = p_test.poll()
+    if test_ret != 0:
+      print 'FAILED'
+      print 'Output:'
+      print test_output
+    else:
+      print '\t\tsuccess'
